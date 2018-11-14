7562756b1b6697ca23c6a5a2b2e77d7f94219b09
fanc <fanc@fancs-MacBook-Pro.local>
2018-11-14 22:12:44 +0800

    11.14打卡

--- PYTHON/复杂链表的复制.py
+++ PYTHON/复杂链表的复制.py
@@ -11,7 +11,6 @@ class Solution:
         while pNode:
             CloneNode = RandomListNode(pNode.label)
             CloneNode.next = pNode.next
-            CloneNode.random = None
             pNode.next = CloneNode
             pNode = CloneNode.next
         return pHead
@@ -25,30 +24,32 @@ class Solution:
             if pNode:
                 CloneNode = pNode.next
     def Separate(self, pHead):
-        CloneHead = pHead.next
-        CloneNode = CloneHead
-        while CloneNode:
-            if CloneNode.next:
-                CloneNode.next = CloneNode.next.next
-            CloneNode = CloneNode.next
-        return CloneHead
+        # CloneHead = pHead.next
+        # # CloneNode = CloneHead
+        # while CloneNode:
+        #     if CloneNode.next:
+        #         CloneNode.next = CloneNode.next.next
+        #     CloneNode = CloneNode.next
+        # return CloneHead
+        pNode = pHead
+        pCloneHead = None
+        pCloneNode = None
+        if pNode != None:
+            pCloneHead = pCloneNode = pNode.next
+            pNode.next = pCloneNode.next
+            pNode = pNode.next
+        while pNode != None:
+            pCloneNode.next = pNode.next
+            pCloneNode = pCloneNode.next
+            pNode.next = pCloneNode.next
+            pNode = pNode.next
+        return pCloneHead
     def Clone(self, pHead):
         # write code here
         if pHead == None:
             return None
         self.CloneNodes(pHead)
-        print(pHead.label, pHead.next.label, pHead.next.next.label, pHead.next.next.next.label)
         self.Setrandom(pHead)
         return self.Separate(pHead)
 pHead = RandomListNode(1)
-pHead.next = RandomListNode(2)
-pHead.next.next = RandomListNode(3)
-pHead.random = pHead.next.next
-pHead.next.random = pHead
-newHead = Solution().Clone(pHead)
-print(newHead.label, newHead.next.label, newHead.next.next.label)
-print(newHead.random.label)
-print(newHead.next.random.label)
-print(newHead.next.next.random)
-print(pHead, pHead.next, pHead.next.next)
-print(newHead, newHead.next, newHead.next.next)
+print(Solution().CloneNodes(pHead).label)
